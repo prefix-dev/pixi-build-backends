e3950d5e8f87ed0659bdccf9e3893da920e1063c
nichmor <nmorkotilo@gmail.com>
2025-03-18 17:06:59 +0200

    feat: add rust backend

---
+++ crates/pixi-build-rust/src/build_script.rs
@@ -0,0 +1,28 @@
+
+use minijinja::Environment;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct BuildScriptContext {
+    pub build_platform: BuildPlatform,
+    pub source_dir: String,
+    pub extra_args: Vec<String>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum BuildPlatform {
+    Windows,
+    Unix,
+}
+
+impl BuildScriptContext {
+    pub fn render(&self) -> Vec<String> {
+        let env = Environment::new();
+        let template = env
+            .template_from_str(include_str!("build_script.j2"))
+            .unwrap();
+        let rendered = template.render(self).unwrap().to_string();
+        rendered.lines().map(|s| s.to_string()).collect()
+    }
+}
--- crates/pixi-build-rust/src/rust.rs
+++ crates/pixi-build-rust/src/rust.rs
@@ -1,53 +1,52 @@
-use std::{
-    collections::BTreeMap,
-    path::{Path, PathBuf},
-    str::FromStr,
-};
+use std::{collections::BTreeMap, path::PathBuf, str::FromStr};
 
 use miette::IntoDiagnostic;
 use pixi_build_backend::{
     common::{requirements, BuildConfigurationParams},
     compilers::default_compiler,
-    traits::{project::new_spec, Dependencies},
+    ProjectModel,
 };
-use pixi_build_backend::{ProjectModel, Targets};
 use rattler_build::{
     console_utils::LoggingOutputHandler,
     hash::HashInfo,
     metadata::{BuildConfiguration, PackagingSettings},
     recipe::{
-        parser::{Build, Dependency, Package, Requirements, ScriptContent},
+        parser::{
+            Build, Dependency, Package, Requirements, ScriptContent,
+        },
         variable::Variable,
         Recipe,
     },
     NormalizedKey,
 };
 use rattler_conda_types::{
-    package::ArchiveType, ChannelConfig, MatchSpec, NoArchType, PackageName, Platform,
+    package::ArchiveType,
+    ChannelConfig, MatchSpec, NoArchType, PackageName, Platform,
 };
 use rattler_package_streaming::write::CompressionLevel;
 
 use crate::{
     build_script::{BuildPlatform, BuildScriptContext},
-    config::CMakeBackendConfig,
+    config::RustBackendConfig,
 };
 
-pub struct CMakeBuildBackend<P: ProjectModel> {
+pub struct RustBuildBackend<P: ProjectModel> {
     pub(crate) logging_output_handler: LoggingOutputHandler,
     pub(crate) manifest_path: PathBuf,
     pub(crate) manifest_root: PathBuf,
     pub(crate) project_model: P,
-    pub(crate) config: CMakeBackendConfig,
+    pub(crate) config: RustBackendConfig,
     pub(crate) cache_dir: Option<PathBuf>,
+    // pub(crate) pyproject_manifest: Option<PyProjectToml>,
 }
 
-impl<P: ProjectModel> CMakeBuildBackend<P> {
-    /// Returns a new instance of [`CMakeBuildBackend`] by reading the manifest
+impl<P: ProjectModel> RustBuildBackend<P> {
+    /// Returns a new instance of [`PythonBuildBackend`] by reading the manifest
     /// at the given path.
     pub fn new(
-        manifest_path: &Path,
+        manifest_path: PathBuf,
         project_model: P,
-        config: CMakeBackendConfig,
+        config: RustBackendConfig,
         logging_output_handler: LoggingOutputHandler,
         cache_dir: Option<PathBuf>,
     ) -> miette::Result<Self> {
@@ -67,44 +66,37 @@ impl<P: ProjectModel> CMakeBuildBackend<P> {
         })
     }
 
-    /// Returns the matchspecs for the compiler packages. That should be
-    /// included in the build section of the recipe.
+    /// Returns the matchspecs for the compiler packages.
+    /// That should be included in the build section of the recipe.
+    /// TODO: Should we also take into account other compilers like
+    /// c or cxx?
     fn compiler_packages(&self, target_platform: Platform) -> Vec<MatchSpec> {
         let mut compilers = vec![];
 
-        for lang in self.languages() {
-            if let Some(name) = default_compiler(target_platform, &lang) {
-                // TODO: Read this from variants
-                // TODO: Read the version specification from variants
-                let compiler_package =
-                    PackageName::new_unchecked(format!("{name}_{target_platform}"));
-                compilers.push(MatchSpec::from(compiler_package));
-            }
-
-            // TODO: stdlib??
+        if let Some(name) = default_compiler(target_platform, "rust") {
+            // TODO: Read this from variants
+            // TODO: Read the version specification from variants
+            let compiler_package = PackageName::new_unchecked(format!("{name}_{target_platform}"));
+            compilers.push(MatchSpec::from(compiler_package));
         }
 
         compilers
     }
 
-    /// Returns the languages that are used in the cmake project. These define
-    /// which compilers are required to build the project.
-    fn languages(&self) -> Vec<String> {
-        // TODO: Can we figure this out from looking at the CMake?
-        vec!["cxx".to_string()]
-    }
-
-    /// Constructs a [`Recipe`] from the current manifest. The constructed
-    /// recipe will invoke CMake to build and install the package.
+    /// Constructs a [`Recipe`] that will build the python package into a conda
+    /// package.
+    ///
+    ///
+    /// Script entry points are read from the pyproject and added as entry
+    /// points in the conda package.
     pub(crate) fn recipe(
         &self,
         host_platform: Platform,
         channel_config: &ChannelConfig,
         variant: &BTreeMap<NormalizedKey, Variable>,
     ) -> miette::Result<Recipe> {
-        // Parse the package name from the manifest
-        let project_model = &self.project_model;
-        let name = PackageName::from_str(project_model.name()).into_diagnostic()?;
+        // Parse the package name and version from the manifest
+        let name = PackageName::from_str(self.project_model.name()).into_diagnostic()?;
         let version = self.project_model.version().clone().ok_or_else(|| {
             miette::miette!("a version is missing from the package but it is required")
         })?;
@@ -129,45 +121,23 @@ impl<P: ProjectModel> CMakeBuildBackend<P> {
 
         Ok(Recipe {
             schema_version: 1,
-            context: Default::default(),
             package: Package {
                 version: version.into(),
                 name,
             },
+            context: Default::default(),
             cache: None,
-            // source: vec![Source::Path(PathSource {
-            //     // TODO: How can we use a git source?
-            //     path: manifest_root.to_path_buf(),
-            //     sha256: None,
-            //     md5: None,
-            //     patches: vec![],
-            //     target_directory: None,
-            //     file_name: None,
-            //     use_gitignore: true,
-            // })],
-            // We hack the source location
+            // Sometimes rust projects could be a part of a workspace, so we need to
+            // include entire source project
+            // and set the source directory to the root of the package.
             source: vec![],
             build: Build {
                 number: build_number,
                 string: Default::default(),
-
-                // skip: Default::default(),
                 script: ScriptContent::Commands(build_script).into(),
                 noarch: noarch_type,
-
-                // TODO: Python is not exposed properly
-                //python: Default::default(),
-                // dynamic_linking: Default::default(),
-                // always_copy_files: Default::default(),
-                // always_include_files: Default::default(),
-                // merge_build_and_host_envs: false,
-                // variant: Default::default(),
-                // prefix_detection: Default::default(),
-                // post_process: vec![],
-                // files: Default::default(),
                 ..Build::default()
             },
-            // TODO read from manifest
             requirements,
             tests: vec![],
             about: Default::default(),
@@ -184,10 +154,6 @@ impl<P: ProjectModel> CMakeBuildBackend<P> {
         let project_model = &self.project_model;
         let dependencies = project_model.dependencies(Some(host_platform));
 
-        let build_tools = build_tools();
-        let empty_spec = new_spec::<P>();
-        let dependencies = add_build_tools::<P>(dependencies, &build_tools, &empty_spec);
-
         let mut requirements = requirements::<P>(dependencies, channel_config, variant)?;
 
         requirements.build.extend(
@@ -200,30 +166,7 @@ impl<P: ProjectModel> CMakeBuildBackend<P> {
     }
 }
 
-/// Returns the build tools that are required to build the project.
-pub(crate) fn build_tools() -> Vec<String> {
-    vec!["cmake".to_string(), "ninja".to_string()]
-}
-
-/// Adds the build tools to the dependencies.
-pub(crate) fn add_build_tools<'a, P: ProjectModel>(
-    mut dependencies: Dependencies<'a, <P::Targets as Targets>::Spec>,
-    build_tools: &'a [String],
-    empty_spec: &'a <<P as ProjectModel>::Targets as Targets>::Spec,
-) -> Dependencies<'a, <<P as ProjectModel>::Targets as Targets>::Spec> {
-    for pkg_name in build_tools.iter() {
-        if dependencies.build.contains_key(pkg_name) {
-            // If the host dependencies already contain the package, we don't need to add it
-            // again.
-            continue;
-        }
-
-        dependencies.build.insert(pkg_name, empty_spec);
-    }
-
-    dependencies
-}
-
+/// Construct a build configuration for the given recipe and parameters.
 pub(crate) fn construct_configuration(
     recipe: &Recipe,
     params: BuildConfigurationParams,
@@ -252,20 +195,63 @@ pub(crate) fn construct_configuration(
 
 #[cfg(test)]
 mod tests {
+
     use std::{collections::BTreeMap, path::PathBuf};
 
     use pixi_build_type_conversions::to_project_model_v1;
+
     use pixi_manifest::Manifests;
-    use rattler_build::console_utils::LoggingOutputHandler;
+    use rattler_build::{console_utils::LoggingOutputHandler, recipe::Recipe};
     use rattler_conda_types::{ChannelConfig, Platform};
     use tempfile::tempdir;
 
-    use crate::{cmake::CMakeBuildBackend, config::CMakeBackendConfig};
+    use crate::{config::RustBackendConfig, rust::RustBuildBackend};
+
+    fn recipe(manifest_source: &str, config: RustBackendConfig) -> Recipe {
+        let tmp_dir = tempdir().unwrap();
+        let tmp_manifest = tmp_dir.path().join("pixi.toml");
+        std::fs::write(&tmp_manifest, manifest_source).unwrap();
+        let manifest = Manifests::from_workspace_manifest_path(tmp_manifest.clone()).unwrap();
+        let package = manifest.value.package.unwrap();
+        let channel_config = ChannelConfig::default_with_root_dir(tmp_dir.path().to_path_buf());
+        let project_model = to_project_model_v1(&package.value, &channel_config).unwrap();
+
+        let python_backend = RustBuildBackend::new(
+            tmp_manifest,
+            project_model,
+            config,
+            LoggingOutputHandler::default(),
+            None,
+        )
+        .unwrap();
+
+        python_backend
+            .recipe(Platform::current(), &channel_config, &BTreeMap::new())
+            .unwrap()
+    }
+
+    #[test]
+    fn test_rust_is_in_build_requirements() {
+        insta::assert_yaml_snapshot!(recipe(r#"
+        [workspace]
+        platforms = []
+        channels = []
+        preview = ["pixi-build"]
+
+        [package]
+        name = "foobar"
+        version = "0.1.0"
+
+        [package.build]
+        backend = { name = "pixi-build-rust", version = "*" }
+        "#, RustBackendConfig::default()), {
+            ".source[0].path" => "[ ... path ... ]",
+            ".build.script" => "[ ... script ... ]",
+        });
+    }
 
     #[tokio::test]
     async fn test_setting_host_and_build_requirements() {
-        // get cargo manifest dir
-
         let package_with_host_and_build_deps = r#"
         [workspace]
         name = "test-reqs"
@@ -275,7 +261,7 @@ mod tests {
 
         [package]
         name = "test-reqs"
-        version = "1.0"
+        version = "1.2.3"
 
         [package.host-dependencies]
         hatchling = "*"
@@ -284,10 +270,10 @@ mod tests {
         boltons = "*"
 
         [package.run-dependencies]
-        foobar = "==3.2.1"
+        foobar = ">=3.2.1"
 
         [package.build]
-        backend = { name = "pixi-build-python", version = "*" }
+        backend = { name = "pixi-build-rust", version = "*" }
         "#;
 
         let tmp_dir = tempdir().unwrap();
@@ -296,14 +282,14 @@ mod tests {
         // write the raw string into the file
         std::fs::write(&tmp_manifest, package_with_host_and_build_deps).unwrap();
 
-        let manifest = Manifests::from_workspace_manifest_path(tmp_manifest).unwrap();
+        let manifest = Manifests::from_workspace_manifest_path(tmp_manifest.clone()).unwrap();
         let package = manifest.value.package.unwrap();
         let channel_config = ChannelConfig::default_with_root_dir(tmp_dir.path().to_path_buf());
         let project_model = to_project_model_v1(&package.value, &channel_config).unwrap();
-        let cmake_backend = CMakeBuildBackend::new(
-            &package.provenance.path,
+        let python_backend = RustBuildBackend::new(
+            package.provenance.path,
             project_model,
-            CMakeBackendConfig::default(),
+            RustBackendConfig::default(),
             LoggingOutputHandler::default(),
             None,
         )
@@ -312,47 +298,18 @@ mod tests {
         let channel_config = ChannelConfig::default_with_root_dir(PathBuf::new());
 
         let host_platform = Platform::current();
+        let variant = BTreeMap::new();
 
-        let recipe = cmake_backend.recipe(host_platform, &channel_config, &BTreeMap::new());
-        insta::with_settings!({
-            filters => vec![
-                ("(vs2017|vs2019|gxx|clang).*", "\"[ ... compiler ... ]\""),
-            ]
-        }, {
-            insta::assert_yaml_snapshot!(recipe.unwrap(), {
-               ".build.script" => "[ ... script ... ]",
-            });
+        let reqs = python_backend
+            .requirements(host_platform, &channel_config, &variant)
+            .unwrap();
+
+        insta::assert_yaml_snapshot!(reqs);
+
+        let recipe = python_backend.recipe(host_platform, &channel_config, &BTreeMap::new());
+        insta::assert_yaml_snapshot!(recipe.unwrap(), {
+            ".source[0].path" => "[ ... path ... ]",
+            ".build.script" => "[ ... script ... ]",
         });
-    }
-
-    #[tokio::test]
-    async fn test_parsing_subdirectory() {
-        // a manifest with subdir
-
-        let package_with_git_and_subdir = r#"
-        [workspace]
-        name = "test-reqs"
-        channels = ["conda-forge"]
-        platforms = ["osx-arm64"]
-        preview = ["pixi-build"]
-
-        [package]
-        name = "test-reqs"
-        version = "1.0"
-
-        [package.build]
-        backend = { name = "pixi-build-python", version = "*" }
-
-        [package.host-dependencies]
-        hatchling = { git = "git+https://github.com/hatchling/hatchling.git", subdirectory = "src" }
-        "#;
-
-        let tmp_dir = tempdir().unwrap();
-        let tmp_manifest = tmp_dir.path().join("pixi.toml");
-
-        // write the raw string into the file
-        std::fs::write(&tmp_manifest, package_with_git_and_subdir).unwrap();
-
-        Manifests::from_workspace_manifest_path(tmp_manifest).unwrap();
     }
 }

498534fc7506f412ee565262a6a9031ad96878e2
nichmor <nmorkotilo@gmail.com>
2025-07-22 21:05:15 +0300

    misc: remove commented code

--- py-pixi-build-backend/src/error.rs
+++ py-pixi-build-backend/src/error.rs
@@ -9,33 +9,8 @@ pub enum PyPixiBuildBackendError {
     #[error(transparent)]
     Cli(Box<dyn Error>),
 
-    // #[error("CLI error: {0}")]
-    // Cli(String),
     #[error(transparent)]
     GeneratedRecipe(Box<dyn Error>),
-    // #[error("Failed to serialize/deserialize Python object")]
-    // PythonSerialization,
-
-    // #[error("Invalid URL format: {0}")]
-    // InvalidUrl(String),
-
-    // #[error("Failed to append item to Python list")]
-    // PythonListAppend,
-
-    // #[error("Failed to convert Python type")]
-    // PythonTypeConversion,
-
-    // #[error("Failed to call Python method")]
-    // PythonMethodCall,
-
-    // #[error("Failed to import Python module")]
-    // PythonModuleImport,
-
-    // #[error("Failed to extract Python object")]
-    // PythonObjectExtract,
-
-    // #[error("Failed to get Python attribute")]
-    // PythonAttributeAccess,
 }
 
 fn pretty_print_error(mut err: &dyn Error) -> String {
@@ -60,47 +35,3 @@ impl From<PyPixiBuildBackendError> for PyErr {
 
 create_exception!(exceptions, CliException, PyException);
 create_exception!(exceptions, GeneratedRecipeException, PyException);
-
-// create_exception!(exceptions, PythonSerializationException, PyException);
-// create_exception!(exceptions, InvalidUrlException, PyException);
-// create_exception!(exceptions, PythonListAppendException, PyException);
-// create_exception!(exceptions, PythonTypeConversionException, PyException);
-// create_exception!(exceptions, PythonMethodCallException, PyException);
-// create_exception!(exceptions, PythonModuleImportException, PyException);
-// create_exception!(exceptions, PythonObjectExtractException, PyException);
-// create_exception!(exceptions, PythonAttributeAccessException, PyException);
-// create_exception!(exceptions, InvalidMatchSpecException, PyException);
-// create_exception!(exceptions, InvalidPackageNameException, PyException);
-// create_exception!(exceptions, InvalidUrlException, PyException);
-// create_exception!(exceptions, InvalidChannelException, PyException);
-// create_exception!(exceptions, ActivationException, PyException);
-// create_exception!(exceptions, ParsePlatformException, PyException);
-// create_exception!(exceptions, ParseArchException, PyException);
-// create_exception!(exceptions, FetchRepoDataException, PyException);
-// create_exception!(exceptions, CacheDirException, PyException);
-// create_exception!(exceptions, DetectVirtualPackageException, PyException);
-// create_exception!(exceptions, IoException, PyException);
-// create_exception!(exceptions, SolverException, PyException);
-// create_exception!(exceptions, TransactionException, PyException);
-// create_exception!(exceptions, LinkException, PyException);
-// create_exception!(exceptions, ConvertSubdirException, PyException);
-// create_exception!(exceptions, VersionBumpException, PyException);
-// create_exception!(exceptions, VersionExtendException, PyException);
-// create_exception!(exceptions, ParseCondaLockException, PyException);
-// create_exception!(exceptions, ConversionException, PyException);
-// create_exception!(exceptions, RequirementException, PyException);
-// create_exception!(exceptions, EnvironmentCreationException, PyException);
-// create_exception!(exceptions, ExtractException, PyException);
-// create_exception!(exceptions, ActivationScriptFormatException, PyException);
-// create_exception!(exceptions, GatewayException, PyException);
-// create_exception!(exceptions, InstallerException, PyException);
-// create_exception!(
-//     exceptions,
-//     ParseExplicitEnvironmentSpecException,
-//     PyException
-// );
-// create_exception!(exceptions, ValidatePackageRecordsException, PyException);
-// create_exception!(exceptions, AuthenticationStorageException, PyException);
-// create_exception!(exceptions, ShellException, PyException);
-// create_exception!(exceptions, InvalidHeaderNameException, PyException);
-// create_exception!(exceptions, InvalidHeaderValueError, PyException);

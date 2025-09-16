e50c7355671a6861736d30e2f69d0499cbe800b9
Daniel Claes <dclaes@smart-robotics.nl>
2025-09-16 11:37:01 +0200

    fix: ensure old package is still present

--- backends/pixi-build-ros/tests/test_package_map.py
+++ backends/pixi-build-ros/tests/test_package_map.py
@@ -9,5 +9,6 @@ def test_package_loading(test_data_dir: Path):
     other_package_map = test_data_dir / "other_package_map.yaml"
     result = load_package_map_data([robostack_file, other_package_map])
     assert "new_package" in result
-    assert result["new_package"]["robostack"] == ["new-package"]
-    assert result["alsa-oss"]["robostack"] == ["other-alsa-oss"]
+    assert result["new_package"]["robostack"] == ["new-package"], "Should be added"
+    assert result["alsa-oss"]["robostack"] == ["other-alsa-oss"], "Should be overwritten"
+    assert "zlib" in result, "Should still be present"

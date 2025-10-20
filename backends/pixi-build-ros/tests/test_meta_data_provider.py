f904db1d431e39f38d5a1312c6ed00d6c44b868b
Tim de Jager <tdejager89@gmail.com>
2025-10-20 14:09:23 +0200

    fix: return None for license_file() as it doesn't work currently
    - Modified license_file() method to return None with TODO comment
    - Updated test assertions to expect None instead of filename
    - Removed broken license_file tests
    - Updated snapshots to reflect missing license_file field

--- backends/pixi-build-ros/tests/test_meta_data_provider.py
+++ backends/pixi-build-ros/tests/test_meta_data_provider.py
@@ -17,7 +17,7 @@ def test_metadata_provider(package_xmls: Path):
     assert metadata_provider.description() == "Demo"
     assert metadata_provider.homepage() == "https://test.io/custom_ros"
     assert metadata_provider.repository() == "https://github.com/test/custom_ros"
-    assert metadata_provider.license_file() == "custom_ros.xml"
+    assert metadata_provider.license_file() is None
 
 
 def test_ros_metadata_provider(package_xmls: Path):
@@ -30,7 +30,7 @@ def test_ros_metadata_provider(package_xmls: Path):
     assert metadata_provider.description() == "Demo"
     assert metadata_provider.homepage() == "https://test.io/custom_ros"
     assert metadata_provider.repository() == "https://github.com/test/custom_ros"
-    assert metadata_provider.license_file() == "custom_ros.xml"
+    assert metadata_provider.license_file() is None
 
 
 def test_metadata_provider_raises_on_broken_xml(package_xmls: Path):

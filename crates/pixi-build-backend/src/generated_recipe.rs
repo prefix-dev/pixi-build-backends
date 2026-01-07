daf1390050f32a62e0dc01731b20f0be206157f0
Bas Zalmstra <4995967+baszalmstra@users.noreply.github.com>
2026-01-07 17:13:44 +0100

    update versions

--- crates/pixi-build-backend/src/generated_recipe.rs
+++ crates/pixi-build-backend/src/generated_recipe.rs
@@ -111,7 +111,7 @@ pub enum GenerateRecipeError<MetadataProviderError: Diagnostic + 'static> {
     #[error("There was no version defined for the recipe")]
     NoVersionDefined,
     #[error("An error occurred while querying the {0}")]
-    MetadataProviderError(String, #[source] MetadataProviderError),
+    MetadataProviderError(String, #[diagnostic_source] #[source] MetadataProviderError),
 }
 
 #[derive(Default, Clone)]
